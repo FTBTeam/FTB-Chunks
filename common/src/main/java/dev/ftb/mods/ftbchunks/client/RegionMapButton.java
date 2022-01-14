3862e29fc04155afc48dccaf085eb76bdeb02af4
LatvianModder <latvianmodder@gmail.com>
2022-01-14 12:02:39 +0200

    Added map blurring when zoomed out in large screen

--- common/src/main/java/dev/ftb/mods/ftbchunks/client/RegionMapButton.java
+++ common/src/main/java/dev/ftb/mods/ftbchunks/client/RegionMapButton.java
@@ -7,6 +7,8 @@ import dev.ftb.mods.ftblibrary.icon.Color4I;
 import dev.ftb.mods.ftblibrary.ui.GuiHelper;
 import dev.ftb.mods.ftblibrary.ui.Theme;
 import dev.ftb.mods.ftblibrary.ui.Widget;
+import net.minecraft.client.Minecraft;
+import org.lwjgl.opengl.GL11;
 
 /**
  * @author LatvianModder
@@ -24,6 +26,11 @@ public class RegionMapButton extends Widget {
 		int id = region.getRenderedMapImageTextureId();
 
 		if (region.mapImageLoaded) {
+			RenderSystem.bindTextureForSetup(id);
+			int filter = w * Minecraft.getInstance().getWindow().getGuiScale() < 512D ? GL11.GL_LINEAR : GL11.GL_NEAREST;
+			RenderSystem.texParameter(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, filter);
+			RenderSystem.texParameter(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, filter);
+
 			RenderSystem.setShaderTexture(0, id);
 			GuiHelper.drawTexturedRect(matrixStack, x, y, w, h, Color4I.WHITE, 0F, 0F, 1F, 1F);
 		}

a081ed009ec96c44f75aa83535d13c0ed269e3ac
zach2039 <sumyunguy2007@gmail.com>
2022-07-21 08:45:17 -0500

    Add platform-dependent methods for fetching Protection types

--- common/src/main/java/dev/ftb/mods/ftbchunks/FTBChunksExpected.java
+++ common/src/main/java/dev/ftb/mods/ftbchunks/FTBChunksExpected.java
@@ -1,6 +1,8 @@
 package dev.ftb.mods.ftbchunks;
 
 import dev.architectury.injectables.annotations.ExpectPlatform;
+import dev.ftb.mods.ftbchunks.data.Protection;
+import dev.ftb.mods.ftbteams.event.TeamCollectPropertiesEvent;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 
@@ -9,6 +11,26 @@ import java.util.UUID;
 public class FTBChunksExpected {
     @ExpectPlatform
     public static void addChunkToForceLoaded(ServerLevel level, String modId, UUID owner, int chunkX, int chunkY, boolean add) {
+        throw new AssertionError();
+    }
+
+    @ExpectPlatform
+    public static void getTeamConfigsForPlatform(TeamCollectPropertiesEvent event) {
+        throw new AssertionError();
+    }
+
+    @ExpectPlatform
+    public static Protection getBlockPlaceProtectionForPlatform() {
+        throw new AssertionError();
+    }
+
+    @ExpectPlatform
+    public static Protection getBlockInteractProtectionForPlatform() {
+        throw new AssertionError();
+    }
+
+    @ExpectPlatform
+    public static Protection getBlockBreakProtectionForPlatform() {
         throw new AssertionError();
     }
 }

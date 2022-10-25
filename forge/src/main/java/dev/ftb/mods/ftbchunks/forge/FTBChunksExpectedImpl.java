48c659e09dbc77b1246332c6ff66a102acb075b9
Des Herriott <des.herriott@gmail.com>
2022-10-25 17:14:04 +0100

    just some debug lines

--- forge/src/main/java/dev/ftb/mods/ftbchunks/forge/FTBChunksExpectedImpl.java
+++ forge/src/main/java/dev/ftb/mods/ftbchunks/forge/FTBChunksExpectedImpl.java
@@ -1,5 +1,6 @@
 package dev.ftb.mods.ftbchunks.forge;
 
+import dev.ftb.mods.ftbchunks.FTBChunks;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraftforge.common.world.ForgeChunkManager;
 
@@ -8,5 +9,6 @@ import java.util.UUID;
 public class FTBChunksExpectedImpl {
     public static void addChunkToForceLoaded(ServerLevel level, String modId, UUID owner, int chunkX, int chunkY, boolean add) {
         ForgeChunkManager.forceChunk(level, modId, owner, chunkX, chunkY, add, false);
+        FTBChunks.LOGGER.info(String.format("force chunk: mod=%s owner=%s, c=[%d,%d], load=%s", modId, owner, chunkX, chunkY, add));
     }
 }

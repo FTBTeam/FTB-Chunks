d5dc457464a919a100cb70b2f6311dcad4e64789
Des Herriott <des.herriott@gmail.com>
2022-11-04 13:58:20 +0000

    fix: remember player's original chunk claims when they join a party
    When they leave the party, transfer those claims back to the player.
    
    https://github.com/FTBTeam/FTB-Mods-Issues/issues/4

--- common/src/main/java/dev/ftb/mods/ftbchunks/data/FTBChunksTeamData.java
+++ common/src/main/java/dev/ftb/mods/ftbchunks/data/FTBChunksTeamData.java
@@ -12,6 +12,7 @@ import dev.ftb.mods.ftblibrary.snbt.SNBTCompoundTag;
 import dev.ftb.mods.ftbteams.FTBTeamsAPI;
 import dev.ftb.mods.ftbteams.data.PrivacyMode;
 import dev.ftb.mods.ftbteams.data.Team;
+import dev.ftb.mods.ftbteams.data.TeamType;
 import dev.ftb.mods.ftbteams.property.BooleanProperty;
 import dev.ftb.mods.ftbteams.property.PrivacyProperty;
 import net.minecraft.Util;
@@ -29,12 +30,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * @author LatvianModder
@@ -56,6 +52,8 @@ public class FTBChunksTeamData {
 	public int extraClaimChunks;
 	public int extraForceLoadChunks;
 	public final Set<UUID> forceLoadMembers;
+	// claims owned by team members before they joined the team - to be returned when player leaves a party
+	public final Map<UUID,Set<ChunkDimPos>> originalClaims;
 	private Boolean canForceLoadChunks;
 
 	public int prevChunkX = Integer.MAX_VALUE, prevChunkZ = Integer.MAX_VALUE;
@@ -71,6 +69,7 @@ public class FTBChunksTeamData {
 		extraClaimChunks = 0;
 		extraForceLoadChunks = 0;
 		forceLoadMembers = new HashSet<>();
+		originalClaims = new HashMap<>();
 	}
 
 	@Override
@@ -112,6 +111,26 @@ public class FTBChunksTeamData {
 		}
 
 		return list;
+	}
+
+	public Collection<ClaimedChunk> getOriginalClaims(UUID playerID) {
+		if (!originalClaims.containsKey(playerID)) return Collections.emptyList();
+
+		List<ClaimedChunk> res = new ArrayList<>();
+		for (ChunkDimPos cdp : originalClaims.get(playerID)) {
+			ClaimedChunk cc = manager.getChunk(cdp);
+			// original claim must still be claimed, and by the current team
+			if (cc != null && cc.teamData == this) {
+				res.add(manager.getChunk(cdp));
+			}
+		}
+
+		return res;
+	}
+
+	public void noteOriginalClaims(FTBChunksTeamData originalTeam) {
+		List<ChunkDimPos> claimedChunks = originalTeam.getClaimedChunks().stream().map(cc -> cc.pos).toList();
+		originalClaims.computeIfAbsent(originalTeam.getTeamId(), k -> new HashSet<>()).addAll(claimedChunks);
 	}
 
 	public void updateLimits(ServerPlayer ownerPlayer) {
@@ -321,8 +340,25 @@ public class FTBChunksTeamData {
 
 			chunksListTag.add(o);
 		}
-
 		tag.put("chunks", chunksTag);
+
+		if (team.getType() == TeamType.PARTY && !originalClaims.isEmpty()) {
+			CompoundTag originalClaimsTag = new CompoundTag();
+			originalClaims.forEach((id, posList) -> {
+				CompoundTag playerTag = new CompoundTag();
+				Map<String,ListTag> perDimensionTags = new HashMap<>();
+				posList.forEach(c -> {
+					ListTag l = perDimensionTags.computeIfAbsent(c.dimension.location().toString(), k -> new ListTag());
+					CompoundTag cdpTag = new CompoundTag();
+					cdpTag.putInt("x", c.x);
+					cdpTag.putInt("z", c.z);
+					l.add(cdpTag);
+				});
+				perDimensionTags.forEach(playerTag::put);
+				originalClaimsTag.put(id.toString(), playerTag);
+			});
+			tag.put("original_claims", originalClaimsTag);
+		}
 
 		return tag;
 	}
@@ -361,6 +397,27 @@ public class FTBChunksTeamData {
 				manager.claimedChunks.put(chunk.pos, chunk);
 			}
 		}
+
+		CompoundTag originalClaimsTag = tag.getCompound("original_claims");
+		originalClaims.clear();
+		for (String idStr : originalClaimsTag.getAllKeys()) {
+			try {
+				UUID id = UUID.fromString(idStr);
+				CompoundTag playerTag = originalClaimsTag.getCompound(idStr);
+				for (String dimStr : playerTag.getAllKeys()) {
+					ResourceKey<Level> dimKey = ResourceKey.create(Registry.DIMENSION_REGISTRY, new ResourceLocation(dimStr));
+					Set<ChunkDimPos> cdpSet = new HashSet<>();
+					playerTag.getList(dimStr, Tag.TAG_COMPOUND).forEach(el -> {
+						if (el instanceof CompoundTag c) {
+							cdpSet.add(new ChunkDimPos(dimKey, c.getInt("x"), c.getInt("z")));
+						}
+					});
+					originalClaims.put(id, cdpSet);
+				}
+			} catch (IllegalArgumentException ignored) {
+			}
+		}
+
 	}
 
 	public int getExtraClaimChunks() {
@@ -437,4 +494,5 @@ public class FTBChunksTeamData {
 	public boolean allowExplosions() {
 		return team.getProperty(ALLOW_EXPLOSIONS);
 	}
+
 }

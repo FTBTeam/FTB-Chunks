bd93ff5eb1f51e037bb83d59155c85d01bf60e33
Des Herriott <des.herriott@gmail.com>
2022-12-15 14:39:18 +0000

    fix: resolve chunks not ticking in remote dimensions
    Chunks were forceloaded but not ticking.  Also, cleaned up the forge
    ticket-validation code; just clear out tickets for chunks which are
    not claimed & loaded by the corresponding team, rather than purging
    everything.
    
    https://github.com/FTBTeam/FTB-Mods-Issues/issues/595

--- forge/src/main/java/dev/ftb/mods/ftbchunks/forge/FTBChunksExpectedImpl.java
+++ forge/src/main/java/dev/ftb/mods/ftbchunks/forge/FTBChunksExpectedImpl.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 
 public class FTBChunksExpectedImpl {
     public static void addChunkToForceLoaded(ServerLevel level, String modId, UUID owner, int chunkX, int chunkY, boolean add) {
-        ForgeChunkManager.forceChunk(level, modId, owner, chunkX, chunkY, add, false);
+        ForgeChunkManager.forceChunk(level, modId, owner, chunkX, chunkY, add, true);
         FTBChunks.LOGGER.debug("force chunk (forge): mod={} owner={}, c=({},{}), load={}", modId, owner, chunkX, chunkY, add);
     }
 }

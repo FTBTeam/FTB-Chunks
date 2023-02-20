0f109a1ab7c79c13b8dc43edf3f4c85b66f488dd
Des Herriott <des.herriott@gmail.com>
2023-02-20 10:13:29 +0000

    fix: properly clean up stale non-ticking forge chunk tickets
    https://github.com/FTBTeam/FTB-Mods-Issues/issues/657
    https://github.com/FTBTeam/FTB-Mods-Issues/issues/101

--- forge/src/main/java/dev/ftb/mods/ftbchunks/forge/FTBChunksForge.java
+++ forge/src/main/java/dev/ftb/mods/ftbchunks/forge/FTBChunksForge.java
@@ -1,17 +1,13 @@
 package dev.ftb.mods.ftbchunks.forge;
 
-import com.google.common.collect.Sets;
 import dev.architectury.platform.Platform;
 import dev.architectury.platform.forge.EventBuses;
 import dev.ftb.mods.ftbchunks.FTBChunks;
 import dev.ftb.mods.ftbchunks.compat.waystones.WaystonesCompat;
 import dev.ftb.mods.ftbchunks.data.ClaimedChunk;
 import dev.ftb.mods.ftbchunks.data.FTBChunksAPI;
-import dev.ftb.mods.ftbchunks.data.FTBChunksTeamData;
 import dev.ftb.mods.ftbchunks.data.Protection;
 import dev.ftb.mods.ftblibrary.math.ChunkDimPos;
-import dev.ftb.mods.ftbteams.FTBTeamsAPI;
-import dev.ftb.mods.ftbteams.data.Team;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionResult;
@@ -21,13 +17,11 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.world.ForgeChunkManager;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Mod(FTBChunks.MOD_ID)
 public class FTBChunksForge {
@@ -69,6 +63,17 @@ public class FTBChunksForge {
 	private void validateLoadedChunks(ServerLevel level, ForgeChunkManager.TicketHelper ticketHelper) {
 		// Clean up any ticking Forge chunk-loading tickets for chunks which aren't both claimed and force-loaded
 		ticketHelper.getEntityTickets().forEach((id, chunks) -> {
+			FTBChunks.LOGGER.debug("validating {} ticking chunk tickets for {}", chunks.getSecond().size(), id);
+
+			// non-ticking tickets - shouldn't have any of these; purge just in case (older releases of Chunks registered them)
+			Set<Long> toRemoveNon = new HashSet<>(chunks.getFirst());
+			if (!toRemoveNon.isEmpty()) {
+				toRemoveNon.forEach(l -> ticketHelper.removeTicket(id, l, false));
+				FTBChunks.LOGGER.info("purged {} non-ticking Forge chunkloading tickets for team ID {} in dimension {}",
+						toRemoveNon.size(), id, level.dimension().location());
+			}
+
+			// ticking tickets - purge if the chunk is either unclaimed or should not be offline-force-loaded
 			Set<Long> toRemove = new HashSet<>();
 			chunks.getSecond().forEach(l -> {
 				ClaimedChunk cc = FTBChunksAPI.getManager().getChunk(new ChunkDimPos(level.dimension(), new ChunkPos(l)));
@@ -76,9 +81,9 @@ public class FTBChunksForge {
 					toRemove.add(l);
 				}
 			});
-			toRemove.forEach(l -> ticketHelper.removeTicket(id, l, true));
 			if (!toRemove.isEmpty()) {
-				FTBChunks.LOGGER.info("cleaned up {} stale Forge chunkloading tickets for team ID {} in dimension {}",
+				toRemove.forEach(l -> ticketHelper.removeTicket(id, l, true));
+				FTBChunks.LOGGER.info("cleaned up {} stale ticking Forge chunkloading tickets for team ID {} in dimension {}",
 						toRemove.size(), id, level.dimension().location());
 			}
 		});

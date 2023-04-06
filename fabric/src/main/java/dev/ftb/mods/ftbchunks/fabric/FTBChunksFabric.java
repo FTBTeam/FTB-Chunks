6104a9d67347cd372037c90c25768b49c4bdbd57
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-04-05 22:57:42 -0400

    Workaround Arch's block break not being sufficient
    The BlockEvent.BREAK event from Arch seems to not fire for PlayerBlockBreakEvents.BEFORE which Fabric mods uses for checking if their actions are allowed or not by claims mods.
    
    This PR does a simple solution of registering to the PlayerBlockBreakEvents.BEFORE event on the Fabric side manually. Should be good for compat and work with my Bumblezone mod (I tested in dev with this change and it looks all good)

--- fabric/src/main/java/dev/ftb/mods/ftbchunks/fabric/FTBChunksFabric.java
+++ fabric/src/main/java/dev/ftb/mods/ftbchunks/fabric/FTBChunksFabric.java
@@ -4,11 +4,20 @@ import dev.architectury.platform.Platform;
 import dev.ftb.mods.ftbchunks.FTBChunks;
 import dev.ftb.mods.ftbchunks.compat.waystones.WaystonesCompat;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import net.minecraft.server.level.ServerPlayer;
 
 public class FTBChunksFabric implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		FTBChunks.instance = new FTBChunks();
+
+		PlayerBlockBreakEvents.BEFORE.register((level, player, blockPos, blockState, blockEntity) -> {
+			if (player instanceof ServerPlayer serverPlayer) {
+				return !FTBChunks.blockBreak(level, blockPos, blockState, serverPlayer, null).isFalse();
+			}
+			 return true;
+		});
 
 		if (Platform.isModLoaded("waystones")) {
 			WaystonesCompat.init();

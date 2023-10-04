d5368acb7a03e91d4cb31966773f76113a632607
Des Herriott <des.herriott@gmail.com>
2023-10-04 10:49:32 +0100

    fix: held item desync after cancelling block-right-click
    Ensure offhand item gets sync'd to offhand slot, not selected slot
    
    https://github.com/FTBTeam/FTB-Mods-Issues/issues/965

--- common/src/main/java/dev/ftb/mods/ftbchunks/FTBCUtils.java
+++ common/src/main/java/dev/ftb/mods/ftbchunks/FTBCUtils.java
@@ -4,6 +4,7 @@ import dev.architectury.injectables.annotations.ExpectPlatform;
 import net.minecraft.network.protocol.game.ClientboundContainerSetSlotPacket;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.level.block.Block;
 
 public class FTBCUtils {
@@ -13,16 +14,19 @@ public class FTBCUtils {
 	}
 
 
-
 	/**
 	 * Used after various events have been cancelled server-side; client may already have updated the held item for the
 	 * player, but it needs to be brought back in sync with the server.
-	 * @param sp the player
+	 *
+	 * @param sp   the player
 	 * @param hand the hand being used
 	 */
 	public static void forceHeldItemSync(ServerPlayer sp, InteractionHand hand) {
 		if (sp.connection != null) {
-			sp.connection.send(new ClientboundContainerSetSlotPacket(-2, 0, sp.getInventory().selected, sp.getItemInHand(hand)));
+			switch (hand) {
+				case MAIN_HAND -> sp.connection.send(new ClientboundContainerSetSlotPacket(-2, 0, sp.getInventory().selected, sp.getItemInHand(hand)));
+				case OFF_HAND -> sp.connection.send(new ClientboundContainerSetSlotPacket(-2, 0, Inventory.SLOT_OFFHAND, sp.getItemInHand(hand)));
+			}
 		}
 	}
 }

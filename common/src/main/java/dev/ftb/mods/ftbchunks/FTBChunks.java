05323e99fe0f5b5793333a2e4d27faf1cc0a77e9
Des Herriott <des.herriott@gmail.com>
2026-02-25 14:14:31 +0000

    fix: sync a player's claim limits to them when they leave a party
    https://github.com/FTBTeam/FTB-Mods-Issues/issues/1974

--- common/src/main/java/dev/ftb/mods/ftbchunks/FTBChunks.java
+++ common/src/main/java/dev/ftb/mods/ftbchunks/FTBChunks.java
@@ -507,6 +507,7 @@ public class FTBChunks {
 
 			if (event.getPlayer() != null) {
 				PlayerVisibilityPacket.syncToLevel(event.getPlayer().level());
+				SendGeneralDataPacket.send(playerData, event.getPlayer());
 			}
 		});
 	}

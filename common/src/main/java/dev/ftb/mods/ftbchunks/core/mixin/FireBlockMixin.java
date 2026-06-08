53fec192fac3ff6ed8503511a442ace3a4b95c61
Des Herriott <des.herriott@gmail.com>
2026-06-08 10:08:29 +0100

    fix: FireBlock mixin application failure on Fabric
    Also using WrapOperation now instead of Redirect
    
    https://github.com/FTBTeam/FTB-Mods-Issues/issues/2103

--- common/src/main/java/dev/ftb/mods/ftbchunks/core/mixin/FireBlockMixin.java
+++ common/src/main/java/dev/ftb/mods/ftbchunks/core/mixin/FireBlockMixin.java
@@ -1,5 +1,7 @@
 package dev.ftb.mods.ftbchunks.core.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import dev.ftb.mods.ftbchunks.util.FireSpreadHelper;
 import net.minecraft.core.BlockPos;
@@ -8,16 +10,17 @@ import net.minecraft.world.level.block.FireBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(FireBlock.class)
 public abstract class FireBlockMixin {
     @Shadow
     protected abstract int getIgniteOdds(LevelReader level, BlockPos pos);
 
-    @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/FireBlock;getIgniteOdds(Lnet/minecraft/world/level/LevelReader;Lnet/minecraft/core/BlockPos;)I"))
-    private int ftbc$getIgniteOdds(FireBlock instance, LevelReader level, BlockPos testPos, @Local(name = "pos", argsOnly = true) BlockPos pos) {
-        if (FireSpreadHelper.shouldPreventFireSpread(level, pos, testPos)) {
+    // Setting argsOnly = true will cause the mixin application to fail on Fabric, but not on NeoForge.  Go figure.
+    @SuppressWarnings("LocalMayBeArgsOnly")
+    @WrapOperation(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/FireBlock;getIgniteOdds(Lnet/minecraft/world/level/LevelReader;Lnet/minecraft/core/BlockPos;)I"))
+    private int ftbc$tick(FireBlock instance, LevelReader level, BlockPos pos, Operation<Integer> original, @Local(name = "pos") BlockPos origPos) {
+        if (FireSpreadHelper.shouldPreventFireSpread(level, origPos, pos)) {
             return 0;
         }
         return getIgniteOdds(level, pos);

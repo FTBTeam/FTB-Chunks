1baf9423dbdd7bb32452ff50948767b7d23568ea
Des Herriott <des.herriott@gmail.com>
2026-06-08 09:09:52 +0100

    fix: move admin-bypass checks to before fake player checks
    Allows fake players who have the UUID of an admin to operate correctly
    
    https://github.com/FTBTeam/FTB-Mods-Issues/issues/2048

--- common/src/main/java/dev/ftb/mods/ftbchunks/data/ClaimedChunkManagerImpl.java
+++ common/src/main/java/dev/ftb/mods/ftbchunks/data/ClaimedChunkManagerImpl.java
@@ -104,7 +104,7 @@ public class ClaimedChunkManagerImpl implements ClaimedChunkManager {
 
 		level.getChunkSource().save(false);
 
-		FTBChunks.LOGGER.info("Force-loaded %d chunks in %s".formatted(map.size(), level.dimension().location()));
+		FTBChunks.LOGGER.info("Force-loaded {} chunks in {}", map.size(), level.dimension().location());
 	}
 
 	private ChunkTeamDataImpl loadTeamData(Team team) {
@@ -154,12 +154,12 @@ public class ClaimedChunkManagerImpl implements ClaimedChunkManager {
 		ChunkTeamDataImpl data = teamData.get(toDelete.getId());
 
 		if (data != null && toDelete.getMembers().isEmpty()) {
-			FTBChunks.LOGGER.debug("dropping references to empty team " + toDelete.getId());
+            FTBChunks.LOGGER.debug("dropping references to empty team {}", toDelete.getId());
 			teamData.remove(toDelete.getId());
 			try {
 				Files.deleteIfExists(data.getFile());
 			} catch (IOException e) {
-				FTBChunks.LOGGER.error(String.format("can't delete file %s: %s", data.getFile(), e.getMessage()));
+				FTBChunks.LOGGER.error("can't delete file {}: {}", data.getFile(), e.getMessage());
 			}
 		}
 	}
@@ -200,7 +200,10 @@ public class ClaimedChunkManagerImpl implements ClaimedChunkManager {
 
 	@Override
 	public boolean shouldPreventInteraction(@Nullable Entity actor, InteractionHand hand, BlockPos pos, Protection protection, @Nullable Entity targetEntity) {
-		if (!(actor instanceof ServerPlayer player) || FTBChunksWorldConfig.DISABLE_PROTECTION.get() || player.level() == null) {
+		if (!(actor instanceof ServerPlayer player)
+				|| FTBChunksWorldConfig.DISABLE_PROTECTION.get()
+				|| getBypassProtection(player.getUUID()))
+		{
 			return false;
 		}
 
@@ -212,9 +215,7 @@ public class ClaimedChunkManagerImpl implements ClaimedChunkManager {
 		ClaimedChunkImpl chunk = getChunk(new ChunkDimPos(player.level(), pos));
 		if (chunk != null) {
 			ProtectionPolicy policy = protection.getProtectionPolicy(player, pos, hand, chunk, targetEntity);
-			boolean prevented = policy.isOverride() ?
-					policy.shouldPreventInteraction() :
-					!player.isSpectator() && (isFake || !getBypassProtection(player.getUUID()));
+			boolean prevented = policy.isOverride() ? policy.shouldPreventInteraction() : !player.isSpectator() && isFake;
             if (prevented) {
 				PlayerNotifier.notifyWithCooldown(player, Component.translatable("ftbchunks.action_prevented").withStyle(ChatFormatting.GOLD), 2000);
 				if (isFake) {
@@ -226,7 +227,7 @@ public class ClaimedChunkManagerImpl implements ClaimedChunkManager {
 			ProtectionPolicy override = protection.getProtectionPolicy(player, pos, hand, null, targetEntity);
 			if (override.isOverride()) {
 				return override.shouldPreventInteraction();
-			} else if (!isFake && (getBypassProtection(player.getUUID()) || player.isSpectator())) {
+			} else if (!isFake && player.isSpectator()) {
 				return false;
 			}
 			PlayerNotifier.notifyWithCooldown(player, Component.translatable("ftbchunks.need_to_claim_chunk").withStyle(ChatFormatting.GOLD), 2000);
